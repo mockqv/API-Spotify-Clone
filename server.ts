135185507ec7bde9552855cc71a86a4a2af41463
mockjk <mockismymain@gmail.com>
2025-01-21 19:51:20 -0300

    :bricks: refactor: changing the default PORT 3001 to 3000

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import authRoutes from "./src/routes/authRoute";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 authRoutes(app);
 
 app.listen(PORT, () => {
